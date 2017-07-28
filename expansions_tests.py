2f9a28bbe757619ca353d492ccfd2487e6ea8efe
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-07-28 11:09:40 +0100

    fixed expansions

--- expansions_tests.py
+++ expansions_tests.py
@@ -7,11 +7,20 @@ import techlabreactor
 
 class TestExpansions(unittest.TestCase):
 
-    def test_worker_saturation(self):
+    def test_expansions_1(self):
         replay = sc2reader.load_replay("test_replays/frugs_vs_ai.SC2Replay")
 
         natural_timing = techlabreactor.natural_expansion_timing(replay.players[0], replay)
         self.assertEqual(natural_timing, 49)
 
-        third_timing = techlabreactor.natural_expansion_timing(replay.players[0], replay)
+        third_timing = techlabreactor.third_expansion_timing(replay.players[0], replay)
         self.assertEqual(third_timing, 161)
+
+    def test_expansions_2(self):
+        replay = sc2reader.load_replay("test_replays/picur.SC2Replay")
+
+        natural_timing = techlabreactor.natural_expansion_timing(replay.players[0], replay)
+        self.assertEqual(natural_timing, 54)
+
+        third_timing = techlabreactor.third_expansion_timing(replay.players[0], replay)
+        self.assertEqual(third_timing, 209)
