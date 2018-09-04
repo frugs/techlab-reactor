3928a1de7a27ee415e1488b0c20d75108e2f58f4
Hugo Wainwright <hugowainwright@fb.com>
2018-09-04 22:40:18 +0100

    tweaked creep

--- techlabreactor/creep.py
+++ techlabreactor/creep.py
@@ -6,6 +6,8 @@ from sc2reader.objects import Player
 from sc2reader.resources import Replay
 from sc2reader.data import Unit
 
+CREEP_TUMOUR_COOLDOWN_BLIZZARD_SECONDS = 15 * 1.4
+
 
 def _get_creep_tumours(player: Player, replay: Replay) -> Set[Unit]:
     return set([
@@ -27,9 +29,17 @@ def creep_tumours_built_before_second(second: int, player: Player,
 
 def creep_tumour_state_changes(player: Player,
                                replay: Replay) -> List[Tuple[int, int]]:
+    def calculate_start_and_finish(
+            creep_tumour: Unit) -> List[Tuple[int, int]]:
+        start = creep_tumour.started_at
+        finish = creep_tumour.finished_at + CREEP_TUMOUR_COOLDOWN_BLIZZARD_SECONDS * replay.game_fps
+        return [(int(start), 1), (int(finish), -1)]
+
     creep_tumours = _get_creep_tumours(player, replay)
-    creep_tumour_start_and_finish = [[(int(creep_tumour.started_at), 1), (int(
-        creep_tumour.finished_at), -1)] for creep_tumour in creep_tumours]
+    creep_tumour_start_and_finish = [
+        calculate_start_and_finish(creep_tumour)
+        for creep_tumour in creep_tumours
+    ]
     creep_tumour_states: List[Tuple[int, int]] = list(
         itertools.chain(*creep_tumour_start_and_finish))
     creep_tumour_states.sort(key=lambda x: x[0])
